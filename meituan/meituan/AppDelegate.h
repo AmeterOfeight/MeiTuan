aab2967b762c763dbfce73b335b3851d550ba59d
wuzhansheng_glo <666666@qq.com>
2020-01-14 18:15:50 +0800

    初始化项目

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by wuzhansheng on 2020/1/14.
+//  Copyright © 2020 JF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ meituan/meituan/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  meituan
+//
+//  Created by wuzhansheng on 2020/1/14.
+//  Copyright © 2020 JF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
